f7abb361ced4f8912197d4ee943227ff8aa9a3af
momoko shinkawa <makinomomoko0521@gmail.com>
2024-04-25 00:18:23 +0900

    typo修正

--- src/main/java/com/movie/movie/service/MovieService.java
+++ src/main/java/com/movie/movie/service/MovieService.java
@@ -4,7 +4,6 @@ import com.movie.movie.entity.Movie;
 import com.movie.movie.exception.InvalidYearRangeException;
 import com.movie.movie.exception.MovieNotFoundException;
 import com.movie.movie.mapper.MovieMapper;
-import jakarta.validation.constraints.AssertTrue;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -19,39 +18,26 @@ public class MovieService {
         this.movieMapper = movieMapper;
     }
 
-    /**
-     * titleStartsWith, fromYear, toYearの値がnullでない場合、validateYearRangeメソッドを呼び出す
-     * それ以外の場合、findAllメソッドを呼び出す
-     */
     public List<Movie> getMovies(String titleStartsWith, Integer fromYear, Integer toYear) {
         if (titleStartsWith != null || (fromYear != null && toYear != null)) {
-            return this.movieMapper.validateYearRange(fromYear, toYear);
+            return this.movieMapper.findMoviesByTitleOrYear(titleStartsWith, fromYear, toYear);
         } else {
             return findAll();
         }
     }
 
-    //findAllメソッドで全ての映画情報を取得する
     public List<Movie> findAll() {
         return this.movieMapper.findAll();
     }
 
-    /**
-     * isYearRangeValidメソッドでfromYear <= toYearかどうかを検証する
-     * fromYear > toYearの場合、InvalidYearRangeExceptionをスローする
-     */
-    public void validateYearRange(Integer fromYear, Integer toYear) {
-        if (!isYearRangeValid(fromYear, toYear)) {
-            throw new InvalidYearRangeException("Invalid year range: fromYear must be less than or equal to toYear");
+    public List<Movie> findMoviesByTitleOrYear(String titleStartsWith, Integer fromYear, Integer toYear) throws
+            InvalidYearRangeException {
+        if (fromYear != null && toYear != null && fromYear > toYear) {
+            throw new InvalidYearRangeException("invalid year range");
         }
+        return this.movieMapper.findMoviesByTitleOrYear(titleStartsWith, fromYear, toYear);
     }
 
-    @AssertTrue(message = "Invalid year range: fromYear must be less than or equal to toYear")
-    public boolean isYearRangeValid(Integer fromYear, Integer toYear) {
-        return fromYear <= toYear;
-    }
-
-    //findByIdメソッドでidに一致する映画情報を取得する
     public Movie findMovieById(Integer id) throws MovieNotFoundException {
         Optional<Movie> movie = this.movieMapper.findById(id);
         if (movie.isPresent()) {
@@ -61,14 +47,12 @@ public class MovieService {
         }
     }
 
-    //title, releaseYearを持つ映画情報を登録する
     public Movie insert(String title, Integer releaseYear) {
         Movie movie = new Movie(null, title, releaseYear);
         movieMapper.insert(movie);
         return movie;
     }
 
-    //idに一致する映画情報のtitle, releaseYearを更新する
     public void update(Integer id, String title, Integer releaseYear) {
         Optional<Movie> optionalMovie = movieMapper.findById(id);
         if (optionalMovie.isEmpty()) {
@@ -80,7 +64,6 @@ public class MovieService {
         movieMapper.update(movie);
     }
 
-    //idに一致する映画情報を削除する
     public void delete(Integer id) {
         Optional<Movie> movie = this.movieMapper.findById(id);
         if (movie.isEmpty()) {
